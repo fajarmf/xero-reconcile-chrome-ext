139e17e293f82f8f73f576f9685c76959625df9d
Fajar Maulana Firdaus <fajarmf@salestock.id>
2015-12-03 04:11:30 +0700

    add conditional

--- script.js
+++ script.js
@@ -1,8 +1,47 @@
-(function() {
-
-    var okButtons = document.getElementsByClassName("xbtn skip okayButton exclude");
-    for (var i = okButtons.length - 1; i >= 0; i--) {
-        okButtons[i].click()
+function parseRight(details) {
+    var spans = details.getElementsByTagName("span");
+    return {
+        date: spans[0].innerText,
+        name: spans[1].innerText,
+        ref: spans[2].innerText.split("Ref: ")[1],
     };
+}
+
+function parseLeft(details) {
+    var spans = details.getElementsByTagName("span");
+    return {
+        date: spans[0].innerText,
+        name: spans[1].innerText,
+        so: spans[2].innerText,
+        orderId: spans[3].innerText
+    };
+}
+
+function getLeftData(line) {
+    var statement = line.getElementsByClassName("statement no-shadow")[0];
+    var details = statement.getElementsByClassName("details")[0];
+    return parseLeft(details);
+}
+
+function getRightData(line) {
+    var statement = line.getElementsByClassName("statement matched")[0];
+    var details = statement.getElementsByClassName("details")[0];
+    return parseRight(details);
+}
+
+(function() {
+    var reconcileTable = document.getElementById("statementLines");
+    var lines = reconcileTable.getElementsByClassName("line");
+    for (var i = 0; i < lines.length; i++) {
+        var line = lines[i];
+        var leftData = getLeftData(line);
+        var rightData = getRightData(line);
+
+        if (leftData.orderId === rightData.ref) {
+            var okButton = line.getElementsByClassName("okayButton")[0];
+            okButton.click();
+        };
+    };
+
 
 })();
