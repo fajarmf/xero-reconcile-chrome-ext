8d7df56797fd50618b1288344da90ba6752402bb
fajar firdaus <fajarmf@gmail.com>
2015-12-02 15:19:16 +0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+(function() {
+
+    var okButtons = document.getElementsByClassName("xbtn skip okayButton exclude");
+    for (var i = okButtons.length - 1; i >= 0; i--) {
+        okButtons[i].click()
+    };
+
+})();
