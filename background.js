ffd60a3146a06fd967e9dc02a9e7b34370a53833
fajar firdaus <fajarmf@gmail.com>
2015-12-06 06:55:28 +0700

    automaticcally reconcile items with matching storename and date

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
 // listen for our browerAction to be clicked
 chrome.browserAction.onClicked.addListener(function (tab) {
-  // for the current tab, inject the "script.js" file & execute it
-  chrome.tabs.executeScript(tab.ib, {
-    file: 'script.js'
-  });
+    // for the current tab, inject the "script.js" file & execute it
+    chrome.tabs.executeScript(tab.ib, {
+        file: 'script.js'
+    });
 });
--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function parseRight(details) {
     var spans = details.getElementsByTagName("span");
     return {
         date: spans[0].innerText,
-        name: spans[1].innerText,
+        store: spans[1].innerText.split("Store: ")[1].toLowerCase(),
         ref: spans[2].innerText.split("Ref: ")[1],
     };
 }
@@ -11,9 +11,9 @@ function parseLeft(details) {
     var spans = details.getElementsByTagName("span");
     return {
         date: spans[0].innerText,
-        name: spans[1].innerText,
-        so: spans[2].innerText,
-        orderId: spans[3].innerText
+        // name: spans[1].innerText,
+        // so: spans[2].innerText,
+        // orderId: spans[3].innerText
     };
 }
 
@@ -25,23 +25,56 @@ function getLeftData(line) {
 
 function getRightData(line) {
     var statement = line.getElementsByClassName("statement matched")[0];
-    var details = statement.getElementsByClassName("details")[0];
-    return parseRight(details);
+    if (statement) {
+        var details = statement.getElementsByClassName("details")[0];
+        return parseRight(details);
+    };
 }
 
-(function() {
+function goToPreviousPage() {
+    var currentPage = window.location.search.split("page=")[1] || 1;
+    if (currentPage > 1) {
+        var li = document.getElementsByClassName("prevPage")[0];
+        var previousPageLink = li.getElementsByTagName("a")[0];
+        previousPageLink.click();
+    } else {
+        return false;
+    }
+}
+
+function runCheck(storeName) {
     var reconcileTable = document.getElementById("statementLines");
     var lines = reconcileTable.getElementsByClassName("line");
+    var counter = 0;
     for (var i = 0; i < lines.length; i++) {
         var line = lines[i];
         var leftData = getLeftData(line);
         var rightData = getRightData(line);
 
-        if (leftData.orderId === rightData.ref) {
+        if (rightData && leftData.date === rightData.date && rightData.store === storeName) {
             var okButton = line.getElementsByClassName("okayButton")[0];
             okButton.click();
+            counter++;
         };
     };
+    if (counter == 0) {
+        // alert("There is no match.");
+    };
+    return counter;
+}
 
+function oke2in() {
+    var storeName = "grand metropolitan";
+    var found = runCheck(storeName);
+    if (found > 0) {
+        setTimeout(oke2in, 5000);
+    } else {
+        if (goToPreviousPage() == false) {
+            alert("All done!");
+        };
+    }
+}
 
+(function() {
+    setTimeout(oke2in, 5000);
 })();
